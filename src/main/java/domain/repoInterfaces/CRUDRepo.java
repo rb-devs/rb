81a4fcf92df240db9020463dddd3265b3b1c655e
evelina <coffecom@yandex.ru>
2019-03-16 19:40:05 +0300

    new CRUD

--- src/main/java/domain/repoInterfaces/CRUDRepo.java
+++ src/main/java/domain/repoInterfaces/CRUDRepo.java
@@ -1,13 +1,11 @@
 package domain.repoInterfaces;
 
 interface CRUDRepo<T> {
-    void add(T model);
+    void addOrUpdate(T model);
 
     void delete(T model);
 
     void delete(short ID);
 
     T get(short ID);
-
-    void update(T model);
 }
